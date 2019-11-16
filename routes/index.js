a1ff93951b078da51ace438d5565fcf2d7127fd5
benny <benny.villiger@eps.ch>
2019-11-16 19:43:52 +0100

    init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const app = express();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+//
+app.listen(8080, function(){
+  console.log("test");
 });
 
 module.exports = router;

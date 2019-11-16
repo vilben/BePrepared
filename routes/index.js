113ee9a36b77a9ee4917f918da408eab49b87a06
benny <benny.villiger@eps.ch>
2019-11-16 19:55:15 +0100

    first attempt

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+const path = require('path');
 const app = express();
 
+app.use(express.static(__dirname));
+
+
 /* GET home page. */
+
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.sendFile(path.join(__dirname+'/../public/html/index.html'));
+    // res.render('index', { title: 'Express' });
 });
-//
-app.listen(8080, function(){
-  console.log("test");
+
+app.use('/', router);
+app.listen(8080, () =>{
+    console.log("test");
 });
+
 
 module.exports = router;

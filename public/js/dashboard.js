9a506764616ec969469a1a8932ceabc3676600d9
benny <benny.villiger@eps.ch>
2019-11-17 00:32:41 +0100

    dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,18 +1,21 @@
-$(document).ready(() =>{
-    $('#showFoodList').click(()=>{
+$(document).ready(() => {
+
+    let $mainContent = $('#mainContent');
+    let baseUrl = window.location.origin;
+
+    $('#showFoodList').click(() => {
         console.log("yippe");
 
-        $.get("http://localhost:8080/checkFood").done((response)=>{
+        $.get(baseUrl +"/checkFood").done((response) => {
             console.log("test", response);
         })
-
     });
 
-
-
-
-
-
+    $('#showUsers').click(() => {
+        $.get(baseUrl +"/userOverview").done((response) => {
+            $mainContent.replaceWith(response);
+        })
+    })
 
 
 });
--- routes/index.js
+++ routes/index.js
@@ -10,30 +10,23 @@ app.use('/', express.static(projectRoot));
 
 const FoodRecognition = require(publicRoute + '/FoodRecognition');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.sendFile(path.join(publicRoute + '/html/index.html'));
 });
-router.get('/dashboard', function(req, res, next) {
+router.get('/dashboard', function (req, res, next) {
     res.sendFile(path.join(publicRoute + '/html/dashboard.html'));
 });
-router.get('/userOverview', function(req, res, next) {
+router.get('/userOverview', function (req, res, next) {
     res.sendFile(path.join(publicRoute + '/html/userOverview.html'));
-    // res.render('index', { title: 'Express' });
 });
-router.get('/userOverview', function(req, res, next) {
-    res.sendFile(path.join(publicRoute + '/html/userOverview.html'));
-    // res.render('index', { title: 'Express' });
-});
-
-router.get('/foodOverView', function(req, res, next) {
+router.get('/foodOverView', function (req, res, next) {
     res.sendFile(path.join(publicRoute + '/html/foodOverView.html'));
-    // res.render('index', { title: 'Express' });
 });
 
-router.get('/checkFood', (req, res, next) =>{
+router.get('/checkFood', (req, res, next) => {
     let pathToBanana = "http://www.pngplay.com/wp-content/uploads/1/Banana-PNG-Royalty-Free.png";
     let foodRecognition = new FoodRecognition();
-    foodRecognition.checkFood(pathToBanana).then((data)=>{
+    foodRecognition.checkFood(pathToBanana).then((data) => {
         res.send(data);
 
         //Todo HACK
@@ -45,7 +38,7 @@ router.get('/checkFood', (req, res, next) =>{
 
 
 app.use('/', router);
-app.listen(8080, () =>{
+app.listen(8080, () => {
     console.log("test");
 });
 module.exports = router;

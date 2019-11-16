86e6e9c156dee8ab0146022283812aedbc3d0b00
Moritz Schiesser <moritzschiesser@gmail.com>
2019-11-16 23:36:07 +0100

    try fixing recognition

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,9 @@ const publicRoute = path.resolve(projectRoot + '/public');
 app.use('public', express.static(publicRoute));
 app.use('/', express.static(projectRoot));
 
-const FoodRecognition = require(publicRoute + '/FoodRecognition.js');
-
 const fs = require('fs');
 const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
+const FoodRecognition = require(publicRoute + '/FoodRecognition');
 
 router.get('/', function(req, res, next) {
     res.sendFile(path.join(publicRoute + '/html/index.html'));

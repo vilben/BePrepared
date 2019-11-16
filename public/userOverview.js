2315bcfcbe1050d4183660a904c1e79d2a1bae57
seiimonn <simon.noser@outlook.com>
2019-11-16 23:12:03 +0100

    Implemented POST/GET for JSON data

--- public/userOverview.js
+++ public/userOverview.js
@@ -1,44 +1,44 @@
 class UserOverview {
 
     constructor(){
-        this.users = []
+        this.users = [];
+        this.loadData();
+        
+    }
 
-        this.loadDemoData()
-        this.showData()
-        this.saveData()
+    addUser(user){
+        this.users.push(user);
+    }
+
+    loadData() {
+        $.get("getUsers").done((response) => {
+            this.users = response.userList;
+            this.showData();
+        });   
+        
     }
 
     saveData(){
-        localStorage.setItem("userData", JSON.stringify(this.users));
+
+        var jsonData = {
+            userList: this.users
+        };
+
+        
+        $.post("getUsers", jsonData).done((response) => {
+            console.log("Wrote to file... maaaybe");
+        });  
+
     }
 
-    loadDemoData(){
-
-        if (localStorage.getItem("userData") === null) {
-            var demoUser1 = new User("Simon", "Noser", "23.01.1998", "m");
-            var demoUser2 = new User("Moritz", "Schiesser", "23.01.1998", "m");
-            var demoUser3 = new User("Benny Joe", "Villiger", "23.01.1998", "m");
-            var demoUser4 = new User("Zuzanna ", "Lottenbach", "23.01.1998", "w");
-            var demoUser5 = new User("Aldin", "Delic", "23.01.1998", "m");
-
-            this.users.push(demoUser1);
-            this.users.push(demoUser2);
-            this.users.push(demoUser3);
-            this.users.push(demoUser4);
-            this.users.push(demoUser5);
-        } else {
-            this.users = JSON.parse(localStorage.getItem("userData"))
-        }
-    }
 
     showData(){
 
         var myTableDiv = $("#userList");
         var table = document.createElement('TABLE');
-        table.setAttribute("class", "grid");
         var tableBody = document.createElement('TBODY');
-        tableBody.setAttribute("class", "gridBody");
-    
+            
+        table.border = '1'
         table.appendChild(tableBody);
             
         var heading = new Array();
@@ -50,7 +50,6 @@ class UserOverview {
         var stock = this.users
            
         var tr = document.createElement('TR');
-        tr.setAttribute("class", "gridHeader")
         tableBody.appendChild(tr);
 
         for (var i = 0; i < heading.length; i++) {
@@ -58,19 +57,12 @@ class UserOverview {
             th.width = '75';
             th.appendChild(document.createTextNode(heading[i]));
             tr.appendChild(th);
+           
         }
-
-        var alternateRow = false;
+            
         for (var i = 0; i < stock.length; i++) {
-            alternateRow = !alternateRow;
 
             var tr = document.createElement('TR');
-            if(alternateRow){
-                tr.setAttribute("class", "gridAlterRow");
-            }
-            else{
-                tr.setAttribute("class", "gridRow");
-            }
             
             var td = document.createElement('TD')
             td.appendChild(document.createTextNode(stock[i].name));
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,21 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser')
+
+
 const projectRoot = path.resolve(__dirname, '../');
 const publicRoute = path.resolve(projectRoot + '/public');
 app.use('public', express.static(publicRoute));
 app.use('/', express.static(projectRoot));
+app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  )
+app.use(bodyParser.json())
+
 router.get('/', function(req, res, next) {
     res.sendFile(path.join(publicRoute + '/html/index.html'));
 });
@@ -25,6 +36,19 @@ router.get('/foodOverView', function(req, res, next) {
     res.sendFile(path.join(publicRoute + '/html/foodOverView.html'));
     // res.render('index', { title: 'Express' });
 });
+
+router.get('/getUsers', function(req, res, next) {
+    res.sendFile(path.join(publicRoute + '/userDummy.json'));
+    // res.render('index', { title: 'Express' });
+});
+
+router.post('/getUsers', function(req, res) {
+    filePath = path.join(publicRoute + '/userDummy.json');
+    fs.writeFile(filePath, JSON.stringify(req.body), function () {
+        res.end();
+    });
+});
+
 app.use('/', router);
 app.listen(8080, () =>{
     console.log("test");

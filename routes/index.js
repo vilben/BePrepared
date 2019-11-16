1c04f8f13de214dd0a879085933bc1163a36ea0d
benny <benny.villiger@eps.ch>
2019-11-16 20:38:13 +0100

    static route

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,22 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 const app = express();
-const publicBasePath = path.join(__dirname + '/../public/');
-app.use(express.static(path.join(__dirname)));
+
+
+const projectRoot = path.resolve(__dirname, '../');
+const publicRoute = path.resolve(projectRoot + '/public');
+
+app.use('public', express.static(publicRoute));
+app.use('/', express.static(projectRoot));
 
 router.get('/', function(req, res, next) {
-    res.sendFile(path.join(publicBasePath + 'html/index.html'));
+    res.sendFile(path.join(publicRoute + '/html/index.html'));
     // res.render('index', { title: 'Express' });
 });
-
-
-
-
-
+router.get('/dashboard', function(req, res, next) {
+    res.sendFile(path.join(publicRoute + '/html/dashboard.html'));
+    // res.render('index', { title: 'Express' });
+});
 
 app.use('/', router);
 

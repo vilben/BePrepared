cac1ece4b9af8dc6e790b05262f1e3405a021771
Moritz Schiesser <moritzschiesser@gmail.com>
2019-11-17 02:22:11 +0100

    working menu

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,30 +1,64 @@
-$(document).ready(() => {
-
+function showDashboard() {
     let $mainContent = $('#mainContent');
     let baseUrl = window.location.origin;
+    setActive("Dashboard");
+    $.get(baseUrl + "/dashboard").done((response) => {
+        $mainContent.replaceWith(response);
+    })
+};
 
+function showFoodList() {
+    let $mainContent = $('#mainContent');
+    let baseUrl = window.location.origin;
+    setActive("FoodList");
+    $.get(baseUrl + "/foodOverView").done((response) => {
+        $mainContent.replaceWith(response);
+        foodOverView.showList();
+    })
+    $.get(baseUrl + "/checkFood").done((response) => {
+        console.log("test", response);
+    })
+};
 
+function showUsers() {
+    let $mainContent = $('#mainContent');
+    let baseUrl = window.location.origin;
+    setActive("Users");
+    $.get(baseUrl + "/userOverview").done((response) => {
+        $mainContent.replaceWith(response);
+        UserOverview.loadData();
+    })
+};
 
-    $('#showFoodList').click(() => {
-        console.log("yippe");
+function showDisaster() {
+    let $mainContent = $('#mainContent');
+    let baseUrl = window.location.origin;   
+    setActive("DisasterSitutation");
+    $.get(baseUrl + "/userOverview").done((response) => {
+        $mainContent.replaceWith(response);
+    })
+};
 
-        $.get(baseUrl +"/checkFood").done((response) => {
-            console.log("test", response);
-        })
-    });
+function setActive(activeTab) {
+    $(".tabPoint").removeClass("active");
 
-    $('#showUsers').click(() => {
-        $.get(baseUrl +"/userOverview").done((response) => {
-            $mainContent.replaceWith(response);
-        })
-    });
+    if (activeTab === "Dashboard") {
+        $("#liDashboard").addClass("active");
+    }
+    else if (activeTab === "FoodList") {
+        $("#liFoodList").addClass("active");
+    }
+    else if (activeTab === "Users") {
+        $("#liUsers").addClass("active");
+    }
+    else if (activeTab === "DisasterSituation") {
+        $("#liDisasterSituation").addClass("active");
+    }
+}
 
-    $('#testButton').click(()=>{
-        $.get(baseUrl + "/takePicture").done((response) =>{
-            console.log(response);
-        })
-    });
-
-
+$('#testButton').click(() => {
+    $.get(baseUrl + "/takePicture").done((response) => {
+        console.log(response);
+    })
 });
 
